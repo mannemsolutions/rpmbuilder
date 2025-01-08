b8828c0580b0bc341f80196a93b2172b3a3d8275
Sebastiaan Mannem <sebas@mannem.nl>
2025-01-08 20:16:07 +0100

    ci: added downloads for wal-g and arm64 for minio

--- scripts/download_rpms.sh
+++ scripts/download_rpms.sh
@@ -3,15 +3,24 @@ set -e
 
 cd "$(dirname "$0")"
 
+function github_release_rpms {
+	ORG=${2:-mannemsolutions}
+	REPO=${1}
+	URL="https://api.github.com/repos/${ORG}/${REPO}/releases/latest"
+	curl -s "${URL}" | jq --raw-output '.assets[] | .browser_download_url'
+}
+
 function minio_rpms {
-	for T in client/mc server/minio; do
-		URL="https://dl.min.io/${T}/release/linux-amd64/"
-		curl -s "${URL}" |
-			grep -oP '".*202.*\.rpm"' |
-			sed 's/"//g' |
-			sort |
-			tail -n 1 |
-			awk '$0="'${URL}'"$0'
+	for ARCH in amd64 arm64; do
+		for T in client/mc server/minio; do
+			URL="https://dl.min.io/${T}/release/linux-${ARCH}/"
+			curl -s "${URL}" |
+				grep -oP '".*202.*\.rpm"' |
+				sed 's/"//g' |
+				sort |
+				tail -n 1 |
+				awk '$0="'${URL}'"$0'
+		done
 	done
 }
 
@@ -25,5 +34,9 @@ function download_rpm {
 cd /host/rpms
 
 minio_rpms | while read -r RPM; do
+	download_rpm "$RPM"
+done
+
+github_release_rpms wal-g-builder | while read -r RPM; do
 	download_rpm "$RPM"
 done
