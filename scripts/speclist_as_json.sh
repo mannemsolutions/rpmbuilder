bf20f023da4b4787f6fc3760b9f5629f95c836e3
Sebastiaan Mannem <sebas@mannem.nl>
2025-03-22 23:06:09 +0100

    chore: Trying to do more in parralel for building all rpms (#26)
    * Trying to do more in parralel for building all rpms

---
+++ scripts/speclist_as_json.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo -n "myspecs="
+{
+	echo "{"
+	echo "\"arch\":[\"arm64\",\"amd64\"],"
+	echo "\"baseimage\":["
+	i=0
+	for img in rockylinux:{8,9} fedora:{39..42}; do
+		i+=1
+		if [ "$i" -gt "1" ]; then
+			echo -n ","
+			echo
+		fi
+		echo -n "\"${img}\""
+	done
+	echo "],"
+	echo "\"specs\":["
+	i=0
+	for spec in specs/*; do
+		i+=1
+		if [ "$i" -gt "1" ]; then
+			echo -n ","
+			echo
+		fi
+		echo -n "\"${spec}\""
+	done
+	echo
+	echo "]}"
+} | jq -c
